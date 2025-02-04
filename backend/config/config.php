670d06487e212c7c2f8a901cb3cc1be2647e27c5
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-04 06:38:31 +0600

    set up jwt utils

---
+++ backend/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'SECRET_KEY' => '4F3^U78gq^v^a7K*',
+    'REFRESH_SECRET_KEY' => '97%9rro%@np2h3!X',
+    'ALGORITHM' => 'HS256',
+    'ACCESS_TOKEN_EXPIRE' => 3600,
+    'REFRESH_TOKEN_EXPIRE' => 86400
+];
---
+++ backend/utils/jwtHelper.php
@@ -0,0 +1,51 @@
+<?php
+require '../vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$config = require '../config/config.php';
+
+define("SECRET_KEY", $config['SECRET_KEY']);
+define("REFRESH_SECRET_KEY", $config['REFRESH_SECRET_KEY'] );
+define("ALGORITHM", $config['ALGORITHM']);
+define("ACCESS_TOKEN_EXPIRE", $config['ACCESS_TOKEN_EXPIRE']);
+define("REFRESH_TOKEN_EXPIRE", $config['REFRESH_TOKEN_EXPIRE']);
+
+function createToken($user, $isRefresh = false){
+    $payload = [
+      'iss' => 'login-system',
+      'iat' => time(),
+      'exp' => time() + ($isRefresh ? REFRESH_TOKEN_EXPIRE : ACCESS_TOKEN_EXPIRE),
+      'sub' => $user['guid'],
+      'email' => $user['email'],
+      'type' => $isRefresh ? 'refresh' : 'access'
+    ];
+
+    $key = $isRefresh ? REFRESH_SECRET_KEY : SECRET_KEY;
+    return JWT::encode($payload, $key, ALGORITHM);
+}
+
+function verifyToken($token, $isRefresh = false){
+    try {
+        $key = $isRefresh ? REFRESH_SECRET_KEY : SECRET_KEY;
+        return (array) JWT::decode($token, new Key($key, ALGORITHM));
+    } catch (Exception $e) {
+        return null;
+    }
+}
+
+
+function blackListToken($token){
+    $blacklistFile = '../storage/blacklist.php';
+    $blacklist = json_decode(file_get_contents($blacklistFile), true) ?: [];
+    $blacklist[] = $token;
+    file_put_contents($blacklistFile, json_encode($blacklist, JSON_PRETTY_PRINT));
+}
+
+
+function isTokenBlacklisted($token){
+    $blacklistFile = '../storage/blacklist.php';
+    $blacklist = json_decode(file_get_contents($blacklistFile), true) ?: [];
+    return in_array($token, $blacklist);
+}
+
