854b27b309eb4697117080e9f5b20272ca377476
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-02-04 09:48:41 +0600

    modified edit endpoint with jwt

--- backend/storage/userData.php
+++ backend/storage/userData.php
@@ -11,7 +11,7 @@
     array (
       'guid' => '3d12b088-086f-4340-b065-29517c43d1bb',
       'email' => 'user1@example.com',
-      'password' => '$2y$10$2VFq7RdWOqnAPp8y6iYBoeQKVJnw/PODY.rZ/1hLQYro0xoZWeAP.',
+      'password' => '$2y$10$R5urHye7Twb079Qz0NLdseDt70E2j4Axp3FoMO0ePakql5uepoZ0e',
     ),
   ),
   'admin' => 
